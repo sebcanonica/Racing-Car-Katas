8a741c0764c9492c57f112002fb564e27be13487
Sébastien Canonica <sebcano.biz@laposte.net>
2020-03-24 13:45:47 +0100

    2nd exercise

--- CSharp/3-TurnTicketDispenser.Tests/TicketDispenserTest.cs
+++ CSharp/3-TurnTicketDispenser.Tests/TicketDispenserTest.cs
@@ -1,6 +1,39 @@
-﻿namespace TDDMicroExercises._3_TurnTicketDispenser
+﻿using FluentAssertions;
+using TDDMicroExercises.TurnTicketDispenser;
+using Xunit;
+
+namespace TDDMicroExercises._3_TurnTicketDispenser
 {
     public class TicketDispenserTest
     {
+        [Fact]
+        public void should_give_ticket_with_expected_number()
+        {
+            TicketDispenser ticketDispenser = new TicketDispenser(new NumberProviderFake(15));
+            TurnTicket turnTicket = ticketDispenser.GetTurnTicket();
+            turnTicket.TurnNumber.Should().Be(15);
+        }        
+    }
+
+    internal class NumberProviderFake : INumberProvider
+    {
+        private int value;
+
+        public NumberProviderFake(int value)
+        {
+            this.value = value;
+        }
+
+        public int GetNextTurnNumber()
+        {
+            return value;
+        }
+    }
+
+    public interface INumberProvider
+    {
+        int GetNextTurnNumber();
     }
 }
+
+
--- CSharp/3-TurnTicketDispenser/TicketDispenser.cs
+++ CSharp/3-TurnTicketDispenser/TicketDispenser.cs
@@ -1,13 +1,33 @@
+using TDDMicroExercises._3_TurnTicketDispenser;
+
 namespace TDDMicroExercises.TurnTicketDispenser
 {
     public class TicketDispenser
     {
+        private INumberProvider numberProvider;
+
+        public TicketDispenser():this(new NumberProvider()) {
+
+        }
+        public TicketDispenser(INumberProvider numberProvider)
+        {
+            this.numberProvider = numberProvider;
+        }
+
         public TurnTicket GetTurnTicket()
         {
-            int newTurnNumber = TurnNumberSequence.GetNextTurnNumber();
+            int newTurnNumber = numberProvider.GetNextTurnNumber();
             TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
 
             return newTurnTicket;
+        }
+    }
+
+    internal class NumberProvider : INumberProvider
+    {
+        public int GetNextTurnNumber()
+        {
+            return TurnNumberSequence.GetNextTurnNumber();
         }
     }
 }
